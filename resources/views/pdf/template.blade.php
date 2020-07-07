009b298a9be803954f8aa6f36d95a46f39d438ae
Ferdiansyah <47508140+ferdiansyah0611@users.noreply.github.com>
2020-07-07 08:17:13 +0700

    Updated resources vue

---
+++ resources/views/pdf/template.blade.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Aloha!</title>
+
+<style type="text/css">
+    * {
+        font-family: Verdana, Arial, sans-serif;
+    }
+    table{
+        font-size: x-small;
+    }
+    tfoot tr td{
+        font-weight: bold;
+        font-size: x-small;
+    }
+    .gray {
+        background-color: lightgray
+    }
+</style>
+
+</head>
+<body>
+<h4 style="text-align: center;">The result of semester 1</h4>
+<h5 style="text-align: center;">2020/2021</h5>
+  <table width="100%">
+    <tr>
+        <td valign="top">
+        	<pre>
+<b>Nama School</b> : WHAt
+<b>Name Student</b> : Ferdi
+<b>Place Student</b> : Jl. APA
+<b>NISN Student</b> : 34892382938
+        	</pre>
+        </td>
+        <td align="right">
+            <h3>Shinra Electric power company</h3>
+            <pre>
+                Company representative name
+                Company address
+                Tax ID
+                phone
+                fax
+            </pre>
+        </td>
+    </tr>
+
+  </table>
+
+  <table width="100%">
+    <tr>
+        <td><strong>From:</strong> Linblum - Barrio teatral</td>
+        <td><strong>To:</strong> Linblum - Barrio Comercial</td>
+    </tr>
+
+  </table>
+
+  <br/>
+
+  <table width="100%">
+    <thead style="background-color: lightgray;">
+      <tr>
+        <th>#</th>
+        <th>Description</th>
+        <th>Quantity</th>
+        <th>Unit Price $</th>
+        <th>Total $</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Playstation IV - Black</td>
+        <td align="right">1</td>
+        <td align="right">1400.00</td>
+        <td align="right">1400.00</td>
+      </tr>
+      <tr>
+          <th scope="row">1</th>
+          <td>Metal Gear Solid - Phantom</td>
+          <td align="right">1</td>
+          <td align="right">105.00</td>
+          <td align="right">105.00</td>
+      </tr>
+      <tr>
+          <th scope="row">1</th>
+          <td>Final Fantasy XV - Game</td>
+          <td align="right">1</td>
+          <td align="right">130.00</td>
+          <td align="right">130.00</td>
+      </tr>
+    </tbody>
+
+    <tfoot>
+        <tr>
+            <td colspan="3"></td>
+            <td align="right">Subtotal $</td>
+            <td align="right">1635.00</td>
+        </tr>
+        <tr>
+            <td colspan="3"></td>
+            <td align="right">Tax $</td>
+            <td align="right">294.3</td>
+        </tr>
+        <tr>
+            <td colspan="3"></td>
+            <td align="right">Total $</td>
+            <td align="right" class="gray">$ 1929.3</td>
+        </tr>
+    </tfoot>
+  </table>
+
+</body>
+</html>
