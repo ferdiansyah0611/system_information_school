1b482789423ae6e89b4e337c183aa526c9b17b02
Ferdiansyah <47508140+ferdiansyah0611@users.noreply.github.com>
2020-07-07 20:30:16 +0700

    Updated AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,7 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Model' => 'App\Policies\ModelPolicy',
+        'App\Model' => 'App\Policies\ModelPolicy',
     ];
 
     /**
@@ -25,7 +25,8 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-        Passport::routes();
+        Passport::routes(null, ['prefix' => 'api/oauth', 'middleware' => array('auth:api', 'web', 'auth')]);
+        Passport::ignoreCsrfToken(true);
         /*if production*/
         /*Passport::loadKeysFrom('/secret-keys/oauth');*/
     }
